2c1ece107447e743f8ac12cbf5f5780ce22c47c0
mohamedzouqqari <zouqqari.m@outlook.fr>
2023-10-12 16:41:32 +0200

    exo

---
+++ exo.js
@@ -0,0 +1,14 @@
+const titi=30*5;
+console.log(titi);
+
+const VB = "hello";
+console.log(VB);
+
+const toto = VB.split("");
+console.log(toto);
+
+const tata = toto.reverse();
+console.log(tata);
+
+const maman = tata.join("");
+console.log(maman);
